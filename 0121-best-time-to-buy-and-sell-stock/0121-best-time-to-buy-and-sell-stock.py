74953c0b7d3c3e4f4e4af44fd8a46ea2edaa7475
DINESHRAJAN T <37593528+DineshRajanT@users.noreply.github.com>
2023-12-22 13:03:15 +0530

    Time: 780 ms (96.93%), Space: 28.1 MB (15.47%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,6 +1,25 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        
+        # T:O(N) 
+        # S:O(N)
+
+        # maxProfit = 0
+        # rightMax = prices[-1]
+        # maxSell = [0] * len(prices)
+
+        # for i in reversed(range(0,len(prices)-1)):
+        #     if prices[i] > rightMax:
+        #         rightMax = prices[i]
+        #     maxSell[i] = rightMax
+
+        # for i in range(len(prices)):
+        #     maxProfit = max(maxProfit, maxSell[i]-prices[i])
+        # return maxProfit
+        #***************************************#
+
+        # T:O(N) 
+        # S:O(1)
+        maxProfit = 0
         rightMax = prices[-1]
         maxSell = [0] * len(prices)
 
@@ -8,10 +27,9 @@ class Solution:
             if prices[i] > rightMax:
                 rightMax = prices[i]
             maxSell[i] = rightMax
-
-        maxProfit = 0
-        for i in range(len(prices)):
             maxProfit = max(maxProfit, maxSell[i]-prices[i])
+
         return maxProfit
+
 
         
