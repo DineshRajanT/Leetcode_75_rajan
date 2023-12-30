b5518f865c391be843829a095a49de264eda9e28
DINESHRAJAN T <37593528+DineshRajanT@users.noreply.github.com>
2023-12-30 22:10:14 +0530

    Time: 22 ms (99.96%), Space: 18.6 MB (17.79%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -3,15 +3,35 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+##### LOOP SOLUTION
+
+# class Solution:
+#     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+#         prevNode = None
+#         currNode = head
+#         while currNode is not None:
+#             nextNode = currNode.next
+#             currNode.next = prevNode
+#             prevNode = currNode
+#             currNode = nextNode
+#         return prevNode # New head
+      
+#### RECURSIVE SOLUTION
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
-        prevNode = None
-        currNode = head
-        while currNode is not None:
-            nextNode = currNode.next
-            currNode.next = prevNode
-            prevNode = currNode
-            currNode = nextNode
-        return prevNode # New head
-      
+        prev = None
+        curr = head
+        
+        def reverse(curr, prev):
+            if curr is None:
+                return prev
+            next = curr.next
+            curr.next = prev
+            return reverse(next, curr)
+            
+        return reverse(curr, prev)
+            
+        
