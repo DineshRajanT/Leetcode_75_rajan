048943e7c1d3701388126f88403917a79cf9666e
DINESHRAJAN T <37593528+DineshRajanT@users.noreply.github.com>
2023-12-30 23:38:14 +0530

    Time: 80 ms (6.83%), Space: 19.8 MB (96.61%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        
+        # Floyd's Cycle detection algo :O(M + N) but still it will leng of LL ==> O(n) linear time
+        slow = head
+        fast = head
+        
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
